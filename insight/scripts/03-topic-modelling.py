ec921f39e5c82722109fd185aaa8640570d7b6b2
stepheli <lisa.i.stephens@gmail.com>
2019-09-16 16:46:02 -0400

    Topic modelling: models alpha and beta added.

--- insight/scripts/03-topic-modelling.py
+++ insight/scripts/03-topic-modelling.py
@@ -1,35 +1,96 @@
 # -*- coding: utf-8 -*-
+# Import packages
 import os
 import pandas as pd
 import numpy as np
-from nltk.stem import SnowballStemmer
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import WhitespaceTokenizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Walkthrough:
-# https://stackabuse.com/python-for-nlp-topic-modeling/
 
-#def text_preprocess(text):
-#    """ 
-#    Pre-processing a text block for LDA. Steps:
-#        (1) Case consistency
-#        (2) Stemming
-#    
-#    Arguments:
-#        text - Unseparated text block of raw text
-#    Ouputs:
-#        text_stemmed - Unseparated text block of stemmed words
-#    """
-#    stemmer = SnowballStemmer("english")
-#    stemmed_text = []
-#    words = text.split(' ')     
-#    
-#    for word in words:
-#        word_lower = word.lower() 
-#        words_stemmed = stemmer.stem(word_lower)
-#        stemmed_text.append(words_stemmed)
-#    stemmed_text = " ".join(stemmed_text)
-#    return stemmed_text
+nltk.tokenize.WhitespaceTokenizer()
+lemmatizer = nltk.stem.WordNetLemmatizer()
+
+# Class definition
+class TopicModelling:
+    def __init__(self,text,text_proc_check,min_df,max_df,n_components,random_state):
+        """ 
+        Initialize class. 
+        
+        Arguments:
+            text - DF column containing text block
+            text_proc_check = Boolean controlling whether text is preprocessed
+            min_df = Minimum number of articles the word must appear in for the
+                word to be considered.
+            max_df = Threshold for unique words to considered (drop words 
+               appearing too frequently, as in stopwords)
+            n_topics = Number of topics to consider
+            random_seed = Random seed to use for the modelling
+        """
+        # Set up internal class variables
+        self.text = text
+        self.text_proc_check = text_proc_check
+        self.min_df = min_df
+        self.max_df = max_df
+        self.n_components = n_components
+        self.random_state = random_state
+        
+        # Pre-process text if requested
+        if self.text_proc_check == True:
+            self.text_preprocess()
+            
+            
+        # Fit an LDA model
+        self.LDA_model, self.word_frequency, self.vocabulary = self.LDA_model()
+    
+    def text_preprocess(self):
+        """ 
+        Arguments:
+            text - pandas series where each entry is the unprocessed text of
+                a given article.
+            
+        Outputs:
+            text_proc - pandas series where the article text has been processed
+                based on the following workflow:
+                    (1) Case consistency (all lowercase)
+                    (2) Lemmatizing
+        """   
+        lemmatizer = WordNetLemmatizer()
+        
+        for article in range(0,len(self.text)):
+            # Print a status update
+            if np.remainder(article,100) == 0:
+                print("Pre-processing requested. Lemmatizing checkpoint {}.".format(article))
+#                print("Original article: {}".format(self.text[article][0:500]))
+            
+            # Process article text, overwrite original text
+            article_text_proc = []
+            article_text = self.text[article].split(" ")
+            for word in article_text:
+                word = word.lower()
+                article_text_proc.append(lemmatizer.lemmatize(word))
+            self.text[article] = " ".join(article_text_proc)
+            
+#            if np.remainder(article,100) == 0:
+#                print("Lemmatized article: {}".format(self.text[article][0:500]))
+            
+    def LDA_model(self):
+        """ Fit text to an LDA model """
+        word_frequency = CountVectorizer(min_df = self.min_df,
+                                         stop_words = 'english')
+        vocabulary = word_frequency.fit_transform(
+                self.text.values.astype('U'))
+        
+        LDA = LatentDirichletAllocation(n_components = self.n_components,
+                                        random_state = self.random_state)
+        LDA_model = LDA.fit(vocabulary)
+        
+        return LDA_model, word_frequency, vocabulary
+
 
 # Import data
 filedir = os.path.dirname(os.path.realpath('__file__'))
@@ -37,50 +98,106 @@ filename = os.path.join('../data/processed/articles_python.csv')
 filename = os.path.abspath(os.path.realpath(filename))
 articles_python = pd.read_csv(filename,index_col = "postId")
 
+# Fit the text to several LDA models for several different settings
 
-word_frequency = CountVectorizer(min_df = 3,
-                                stop_words = 'english')
-vocabulary = word_frequency.fit_transform(articles_python["text"].values.astype('U'))
-
-LDA = LatentDirichletAllocation(n_components=10, random_state=8)
-LDA.fit(vocabulary)
-
-for i, topic in enumerate(LDA.components_):
+# Model Alpha: sklearn/LDA (unsupervised); No pre-processing of text
+model_alpha = TopicModelling(text=articles_python["text"],
+                                       text_proc_check=False,
+                                       min_df = 3,
+                                       max_df = [],
+                                       n_components = 10,
+                                       random_state = 8)
+print('Model alpha:')
+for i, topic in enumerate(model_alpha.LDA_model.components_):
     print('Top words for topic {}:'.format(i))
-    print([word_frequency.get_feature_names()[i] for i in topic.argsort()[-20:]])
+    print([model_alpha.word_frequency.get_feature_names()[i] for i in topic.argsort()[-20:]])
     print('\n')
     
-#for i in range(0,len(articles_python)):
-#    articles_python["text_cleaned"].iloc[i] = text_preprocess(articles_python["text"].iloc[i])
+model_alpha_dict = {0 : 'natural language processing',
+                    1 : 'neural networks',
+                    2 : 'deep learning',
+                    3 : 'machine learning',
+                    4 : 'time series modelling',
+                    5 : 'regressers and classifiers',
+                    6 : 'machine learning',
+                    7 : 'general data science',
+                    8 : 'machine learning',
+                    9 : 'general data science'}
 
-## Extract vocabulary
-#count_vect = CountVectorizer(max_df=0.50, 
-#                             min_df=3, 
-#                             stop_words='english')
-#doc_term_matrix = count_vect.fit_transform(
-#        articles_python["text"].values.astype('U'))
-#
-## Perform LDA
-#LDA = LatentDirichletAllocation(n_components=5, random_state=42)
-#LDA.fit(doc_term_matrix)
-#   
-#for i,topic in enumerate(LDA.components_):
-#    print('Top 20 words for topic #{}:.'.format(i))
-#    print([count_vect.get_feature_names()[i] for i in topic.argsort()[-20:]])
-#    print('\n')
-#    
-#category_labels = {0 : "machine learning / neural networks",
-#                   1 : "machine learning / supervised learning",
-#                   2 : "natural language processing",
-#                   3 : "machine learning / regressors",
-#                   4 : "general data science"}
-#   
-#topic_values = LDA.transform(doc_term_matrix)
-#topic_values.shape
-#
-#articles_python['topic_label'] = topic_values.argmax(axis=1)
-#articles_python.head(50)
-#
-#articles_python = articles_python.assign(topic_name = np.zeros(len(articles_python)),
-#                                             )
-#articles_python['topic_name'] = articles_python["topic_label"].map(category_labels)
+topic_prob_alpha = model_alpha.LDA_model.transform(model_alpha.vocabulary)
+articles_python = articles_python.assign(topic_label_alpha = np.zeros(len(articles_python)))
+articles_python['topic_label_alpha'] = topic_prob_alpha.argmax(axis = 1)
+articles_python['topic_label_alpha'] = articles_python['topic_label_alpha'].map(model_alpha_dict)
+
+for i in range(0,50):
+    print("Article #{}".format(i))
+    print("Title:{}".format(articles_python["title"].iloc[i]))
+    print("Label: {}".format(articles_python["topic_label_alpha"].iloc[i]))
+    print('\n')
+    
+    
+model_alpha_count = articles_python.groupby(["topic_label_alpha"]).size().reset_index(
+        name="counts")
+model_alpha_count = model_alpha_count.sort_values(["counts"],ascending=False)
+print(model_alpha_count.head())
+
+
+# Model Beta: sklearn/LDA (unsupervised); text case consistency + lemmatization
+model_beta = TopicModelling(text=articles_python["text"],
+                                       text_proc_check=True,
+                                       min_df = 3,
+                                       max_df = 0.5,
+                                       n_components = 10,
+                                       random_state = 8)
+
+print('Model beta:')
+for i, topic in enumerate(model_beta.LDA_model.components_):
+    print('Top words for topic {}:'.format(i))
+    print([model_beta.word_frequency.get_feature_names()[i] for i in topic.argsort()[-20:]])
+    print('\n')
+     
+model_beta_dict = {0 : 'machine learning',
+                    1 : 'neural networks',
+                    2 : 'neural networks',
+                    3 : 'general data science',
+                    4 : 'machine learning',
+                    5 : 'machine learning',
+                    6 : 'regressors and classifiers',
+                    7 : 'natural language processing',
+                    8 : 'general data science',
+                    9 : 'general data science'}
+
+topic_prob_beta = model_beta.LDA_model.transform(model_beta.vocabulary)
+articles_python = articles_python.assign(topic_label_beta = np.zeros(len(articles_python)))
+articles_python['topic_label_beta'] = topic_prob_beta.argmax(axis = 1)
+articles_python['topic_label_beta'] = articles_python['topic_label_beta'].map(model_beta_dict)
+
+for i in range(0,50):
+    print("Article #{}".format(i))
+    print("Title:{}".format(articles_python["title"].iloc[i]))
+    print("Label: {}".format(articles_python["topic_label_beta"].iloc[i]))
+    print('\n')
+    
+model_beta_count = articles_python.groupby(["topic_label_beta"]).size().reset_index(
+        name="counts")
+model_beta_count = model_beta_count.sort_values(["counts"],ascending=False)
+print(model_beta_count.head())
+
+# Plot distributions of the topic categorizations for each model
+fig = plt.figure(figsize=(10,6))
+
+ax1 = fig.add_subplot(1,3,1)
+sns.barplot(x = model_alpha_count["topic_label_alpha"],
+            y = model_alpha_count["counts"])
+plt.xticks(rotation=45)
+ax1.set_xlabel("Topic")
+ax1.set_ylabel("Articles")
+        
+ax2 = fig.add_subplot(1,3,2)
+sns.barplot(x = model_beta_count["topic_label_beta"],
+            y = model_beta_count["counts"])
+plt.xticks(rotation=45)
+ax2.set_xlabel("Topic")
+ax2.set_ylabel("Articles")
+    
+fig.subplots_adjust(bottom=0.2)
